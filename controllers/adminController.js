e9347df7e07bcfbc9d361136c69655e9b6f9fae5
Mukund Jee Tiwari <mukundjitiwari@outlook.com>
2023-08-13 10:48:18 +0530

    Updated Files

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -3,6 +3,6 @@ const User = require('../models/user');
 // get the data of dashboard page
 module.exports.dashboard = (req,res) => {
     return res.render('dashboard', {
-        title: req.user.name,
+        title: req.user.username,
     });
 }
--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,14 @@ const router = express.Router();
 const passport = require('passport');
 const homeController = require('../controllers/homeControllers');
 
-router.get('/', homeController.home);
-router.get('/signup', homeController.signUp);
+router.get('/', passport.isAuthenticate, homeController.home);
+router.get('/signup', passport.isAuthenticate, homeController.signUp);
 router.post('/create-user', homeController.create_user);
 router.post('/create-session', passport.authenticate('local', {failureRedirect: '/'}), homeController.create_session);
 router.get('/delete-session', homeController.destroy_session);
 
-router.use('/admin', require('./admin.js'));
-//router.use('/api', require('./api'));
+router.use('/admin', require('./admin'));
+// router.use('/api', require('./api'));
 
 
 module.exports = router;
